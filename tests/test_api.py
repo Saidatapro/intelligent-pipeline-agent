43d579269f34db8d6d1889c096e59558d3d4a085
Saidatapro <boyapatisaiteja565@gmail.com>
2025-11-19 14:56:13 -0700

    Delete tests directory

--- tests/test_api.py
+++
@@ -1,6 +0,0 @@
-
-from fastapi.testclient import TestClient
-from src.api.main import app
-def test_healthz():
-    c=TestClient(app); r=c.get('/healthz')
-    assert r.status_code==200 and r.json().get('ok') is True
